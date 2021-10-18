621b6289ee59e780c7e46a4e12bccb3c88b6f27a
Henrique Souza <carvalhohsouza@gmail.com>
2021-10-18 17:53:45 -0300

    Versão inicial do projeto

---
+++ projeto-hotelaria/src/main/java/br/senac/bangkok/projeto_hotelaria/DB.java
@@ -0,0 +1,20 @@
+package br.senac.bangkok.projeto_hotelaria;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+	public static Connection connect() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(
+				"jdbc:mysql://localhost:3306/bangkok",
+				"root", "Forest*94");
+	}
+	
+	public static void main(String[] args) throws Exception, ClassNotFoundException {
+		connect();
+		System.out.println("conexão OK!");
+	}
+	
+}
